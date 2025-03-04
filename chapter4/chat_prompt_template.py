7ed1e8467ef5db980532c90234290a99f222a308
kakikubo <kakikubo@gmail.com>
2025-03-05 06:25:27 +0900

    PromptTemplate,ChatPromptTemplate,MessagePlaceholder

---
+++ chapter4/chat_prompt_template.py
@@ -0,0 +1,12 @@
+# ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "ユーザーが入力した料理のレシピを考えてください。"),
+        ("human", "{dish}"),
+    ]
+)
+
+prompt_value = prompt.invoke({"dish": "カレー"})
+print(prompt_value)
---
+++ chapter4/message_placeholder.py
@@ -0,0 +1,22 @@
+# MessagesPlaceholder
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant."),
+        MessagesPlaceholder("chat_history", optional=True),
+        ("human", "{input}"),
+    ]
+)
+
+prompt_value = prompt.invoke(
+    {
+        "chat_history": [
+            HumanMessage(content="こんにちは!私はジョンと言います!"),
+            AIMessage("こんにちは、ジョンさん!どのようにお手伝いできますか?"),
+        ],
+        "input": "私の名前がわかりますか?",
+    }
+)
+print(prompt_value)
