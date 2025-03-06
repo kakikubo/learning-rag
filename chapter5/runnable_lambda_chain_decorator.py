52d2cbae90de9b6fa4bf99323294e03501572a1d
kakikubo <kakikubo@gmail.com>
2025-03-07 07:17:59 +0900

    chainデコレーターをつかったRunnableLambdaの実装

---
+++ chapter5/runnable_lambda_chain_decorator.py
@@ -0,0 +1,25 @@
+# chainデコレーターをつかったRunnableLambdaの実装
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_core.runnables import chain
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant."),
+        ("human", "{input}")
+    ]
+)
+
+model = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+
+output_parser = StrOutputParser()
+
+@chain
+def upper(text: str) -> str:
+  return text.upper()
+
+chain = prompt | model | output_parser | upper
+
+output = chain.invoke({"input": "Hello!"})
+print(output)
