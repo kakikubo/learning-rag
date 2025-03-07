b74b98869e6effbe0d09a5300d67ee6df8ca37c2
kakikubo <kakikubo@gmail.com>
2025-03-08 07:18:16 +0900

    RunnableParallel.assignをつかって出力に値を追加する

--- chapter5/assign.py
+++ chapter5/assign.py
@@ -1,10 +1,10 @@
-# RunnablePassthrough - 入力をそのまま出力する
-# runnable_passthrough.py
+# assign - RunnableParallelの出力に値を追加する
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain_community.retrievers import TavilySearchAPIRetriever
 from langchain_core.runnables import RunnablePassthrough
+# from langchain_core.runnables import RunnableParallel
 
 prompt = ChatPromptTemplate.from_template('''\
 以下の文脈だけを踏まえて質問に回答してください。
@@ -18,13 +18,19 @@ prompt = ChatPromptTemplate.from_template('''\
 
 model = ChatOpenAI(model_name="gpt-4o-mini", temperature=0)
 retriever = TavilySearchAPIRetriever(k=3)
+# ここからがrunnable_passthrough.pyとの差分
+import pprint
 
-chain = (
-    {"context": retriever, "question": RunnablePassthrough()}
-    | prompt
-    | model
-    | StrOutputParser()
-)
-
+chain = {
+    "question": RunnablePassthrough(),
+    "context": retriever,
+} | RunnablePassthrough.assign(answer=prompt | model | StrOutputParser())
+# 次のように書くことも可能
+# chain = RunnableParallel(
+#   {
+#     "question": RunnablePassthrough(),
+#     "context": retriever,
+#   },
+# ).assign(answer=prompt | model | StrOutputParser())
 output = chain.invoke("東京の今日の天気は？")
-print(output)
+pprint.pprint(output)
