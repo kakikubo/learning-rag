b600373a95d6534a4794912166c84c3b2f1670ea
kakikubo <kakikubo@gmail.com>
2025-03-09 10:30:39 +0900

    cohereを使ったリランクモデル。ただし、実行にめちゃくちゃ時間がかかる。

--- chapter6/cohere.py
+++ chapter6/cohere.py
@@ -1,12 +1,12 @@
-# 検索後の工夫にRAG-Fusionを使い、複数の検索結果を並べ替えることができる。mutti_query.pyから改修する
+# Cohereのリランクモデルの導入
 from langchain_community.document_loaders import GitLoader
+from langchain_core.documents import Document
 from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_openai import ChatOpenAI
-from pydantic import BaseModel, Field
 
 def file_filter(file_path: str) -> bool:
   return file_path.endswith(".mdx")
@@ -26,7 +26,6 @@ db = Chroma.from_documents(documents, embeddings)
 
 
 # RAGのChain
-
 prompt = ChatPromptTemplate.from_template('''\
 以下の文脈だけを踏まえて質問に回答してください。
 
@@ -38,63 +37,27 @@ prompt = ChatPromptTemplate.from_template('''\
 ''')
 
 model = ChatOpenAI(model="gpt-4o-mini", temperature=0)
-
 retriever = db.as_retriever()
+# Cohereのリランクモデルの導入
+from typing import Any
 
-# 複数の検索クエリを生成するChain
-class QueryGenerationOutput(BaseModel):
-  queries: list[str] = Field(..., description="検索クエリのリスト")
-
-query_generation_prompt = ChatPromptTemplate.from_template("""\
-質問に対してベクターデータベースから関連文書を検索するために、
-3つの異なる検索クエリを生成してください。
-距離ベースの類似性検索の限界を克服するために、
-ユーザーの質問に対して複数の指定を提供することが目標です。
-
-質問: {question}
-""")
-
-query_generation_chain = (
-    query_generation_prompt
-    | model.with_structured_output(QueryGenerationOutput)
-    | (lambda x: x.queries)
-)
-
-# RAG-Fusion
+from langchain_cohere import CohereRerank
 from langchain_core.documents import Document
 
+def rerank(inp: dict[str, Any], top_n: int = 3) -> list[Document]:
+  question = inp["question"]
+  documents = inp["documents"]
 
-# RRF
-def reciprocal_rank_fusion(
-        retriever_outputs: list[list[Document]],
-        k: int = 60,
-) -> list[str]:
-    # 各ドキュメントのコンテンツ(文字列)とそのスコアの対応を保持する辞書を準備
-    content_score_mapping = {}
+  cohere_reranker = CohereRerank(model="rerank-multilingual-v3.0", top_n=top_n) # top_nはリランク結果の上位何件を返すか設定するパラメータ
+  return cohere_reranker.compress_documents(documents=documents, query=question)
 
-    # 検索クエリごとにループ
-    for docs in retriever_outputs:
-        # 検索結果のドキュメントごとにループ
-        for rank, doc in enumerate(docs):
-            content = doc.page_content
+rerank_rag_chain = (
+    {
+        "question": RunnablePassthrough(),
+        "documents": retriever,
+    }
+    | RunnablePassthrough.assign(context=rerank)
+    | prompt | model | StrOutputParser()
+)
 
-            # 初めて登場したコンテンツの場合はスコアを0で初期化
-            if content not in content_score_mapping:
-                content_score_mapping[content] = 0
-
-            # (1 / (順位 + k))のスコアを加算
-            content_score_mapping[content] += 1 / (rank + k)
-
-    # スコアの大きい順にソート
-    ranked = sorted(content_score_mapping.items(), key=lambda x: x[1], reverse=True)
-    return [content for content, _ in ranked]
-
-
-# 上記関数をつかって、RAG-FusionのChain全体を実装
-rag_fusion_chain = {
-                       "question": RunnablePassthrough(),
-                       "context": query_generation_chain | retriever.map() | reciprocal_rank_fusion,
-                   } | prompt | model | StrOutputParser()
-
-output = rag_fusion_chain.invoke("LangChainの概要を教えて")
-print(output)
+rerank_rag_chain.invoke("LangChainの概要を教えて")
